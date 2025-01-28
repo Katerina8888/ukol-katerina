b0b4315325c346f87e382698910352aec852cf88
katerina <katerina@igloonet.cz>
2025-01-28 20:31:50 +0100

    feat: new task creator component as wrapper for input and task

---
+++ src/components/TaskCreator.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Input v-model:taskInput="newTask" @addTask="addTask" />
+    <Task :tasks="tasks" @deleteTask="deleteTask" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import Input from './Input.vue'
+import Task from './Task.vue'
+
+const tasks = ref<string[]>([])
+const newTask = ref('')
+
+const addTask = (task: string) => {
+  tasks.value.push(task)
+}
+
+const deleteTask = (index: number) => {
+  tasks.value.splice(index, 1)
+}
+</script>
