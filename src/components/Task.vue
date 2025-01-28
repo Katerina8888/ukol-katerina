22e9ff64e6f8edb91595cdc941cf00c2a4bbeb38
katerina <katerina@igloonet.cz>
2025-01-28 20:31:08 +0100

    feat: new task component with emit values

---
+++ src/components/Task.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li v-for="(task, index) in tasks" :key="index">
+      <span>{{ task }}</span>
+      <button @click="handleDelete(index)">x</button>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue'
+
+defineProps<{
+  tasks: string[]
+}>()
+
+const emit = defineEmits<{
+  (e: 'delete-task', index: number): void
+}>()
+
+const handleDelete = (index: number) => {
+  emit('delete-task', index)
+}
+</script>
