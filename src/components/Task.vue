0d82881896a9ab7dce9e984436263afe0c70343e
katerina <katerina@igloonet.cz>
2025-01-29 12:04:29 +0100

    refactor: Task component style without logic

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,24 +1,79 @@
 <template>
-  <ul>
-    <li v-for="(task, index) in tasks" :key="index">
-      <span>{{ task }}</span>
-      <button @click="handleDelete(index)">x</button>
-    </li>
-  </ul>
+  <!-- <li v-for="(task, index) in tasks" :key="index"> -->
+  <li class="flex w-full md:w-1/2 lg:w-1/3 flex-col my-4">
+    <div class="w-5/6 md:w-1/2 bg-[#FCC6FF] mx-auto rounded-md p-2">
+      <div class="flex justify-between">
+        <div>1h</div>
+        <h2 class="font-bold">Eat lunch</h2>
+        <ButtonDismiss></ButtonDismiss>
+      </div>
+
+      <div class="text-center">29.1.2025</div>
+    </div>
+    <!-- <button @click="removeTask(index)">x</button> -->
+  </li>
+  <li class="flex w-full md:w-1/2 lg:w-1/3 flex-col my-4">
+    <div class="w-5/6 md:w-1/2 bg-[#FCC6FF] mx-auto rounded-md p-2">
+      <div class="flex justify-between">
+        <div>1h</div>
+        <h2 class="font-bold">Eat lunch</h2>
+        <ButtonDismiss></ButtonDismiss>
+      </div>
+
+      <div class="text-center">29.1.2025</div>
+    </div>
+    <!-- <button @click="removeTask(index)">x</button> -->
+  </li>
+  <li class="flex w-full md:w-1/2 lg:w-1/3 flex-col my-4">
+    <div class="w-5/6 md:w-1/2 bg-[#FCC6FF] mx-auto rounded-md p-2">
+      <div class="flex justify-between">
+        <div>1h</div>
+        <h2 class="font-bold">Eat lunch</h2>
+        <ButtonDismiss></ButtonDismiss>
+      </div>
+
+      <div class="text-center">29.1.2025</div>
+    </div>
+    <!-- <button @click="removeTask(index)">x</button> -->
+  </li>
+  <li class="flex w-full md:w-1/2 lg:w-1/3 flex-col my-4">
+    <div class="w-5/6 md:w-1/2 bg-[#FCC6FF] mx-auto rounded-md p-2">
+      <div class="flex justify-between">
+        <div>1h</div>
+        <h2 class="font-bold">Eat lunch</h2>
+        <ButtonDismiss></ButtonDismiss>
+      </div>
+
+      <div class="text-center">29.1.2025</div>
+    </div>
+    <!-- <button @click="removeTask(index)">x</button> -->
+  </li>
+  <li class="flex w-full md:w-1/2 lg:w-1/3 flex-col my-4">
+    <div class="w-5/6 md:w-1/2 bg-[#FCC6FF] mx-auto rounded-md p-2">
+      <div class="flex justify-between">
+        <div>1h</div>
+        <h2 class="font-bold">Eat lunch</h2>
+        <ButtonDismiss></ButtonDismiss>
+      </div>
+
+      <div class="text-center">29.1.2025</div>
+    </div>
+    <!-- <button @click="removeTask(index)">x</button> -->
+  </li>
+  <li class="flex w-full md:w-1/2 lg:w-1/3 flex-col my-4">
+    <div class="w-5/6 md:w-1/2 bg-[#FCC6FF] mx-auto rounded-md p-2">
+      <div class="flex justify-between">
+        <div>1h</div>
+        <h2 class="font-bold">Eat lunch</h2>
+        <ButtonDismiss></ButtonDismiss>
+      </div>
+
+      <div class="text-center">29.1.2025</div>
+    </div>
+    <!-- <button @click="removeTask(index)">x</button> -->
+  </li>
 </template>
 
 <script setup lang="ts">
-import { defineProps, defineEmits } from 'vue'
-
-defineProps<{
-  tasks: string[]
-}>()
-
-const emit = defineEmits<{
-  (e: 'delete-task', index: number): void
-}>()
-
-const handleDelete = (index: number) => {
-  emit('delete-task', index)
-}
+import ButtonDismiss from './ButtonDismiss.vue'
 </script>
