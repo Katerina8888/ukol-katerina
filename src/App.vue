2b989e51bea029a4971a57e317fd1be782d1b986
katerina <katerina@igloonet.cz>
2025-01-29 12:05:18 +0100

    feat: add TaskWrapper to HP

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
     <TaskCreator />
+    <TasksWrapper />
   </div>
 </template>
 
 <script setup lang="ts">
 import TaskCreator from './components/TaskCreator.vue'
+import TasksWrapper from './components/TasksWrapper.vue'
 </script>
