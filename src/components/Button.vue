085ca7a0067f0318e9dca0d6854d00f7da2bc948
katerina <katerina@igloonet.cz>
2025-01-29 12:03:46 +0100

    feat: new temp ButtonDismiss component - add comment to Button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,3 +1,4 @@
+<!-- TODO SJEDNOTIT S BUTTON A PŘEPÍNAT MEZI VARIANTAMA POMOCÍ PROPS -->
 <template>
   <button
     class="w-16 h-8 rounded-md bg-black text-white text-center mx-auto my-4 hover:text-black hover:bg-white hover:transition-all hover:duration-500 hover:cursor-pointer"
---
+++ src/components/ButtonDismiss.vue
@@ -0,0 +1,8 @@
+<!-- TODO SJEDNOTIT S BUTTON A PŘEPÍNAT MEZI VARIANTAMA POMOCÍ PROPS -->
+<template>
+  <button
+    class="w-4 h-4 text-xs rounded-md bg-black text-white text-center hover:text-black hover:bg-white hover:transition-all hover:duration-500 hover:cursor-pointer"
+  >
+    X
+  </button>
+</template>
