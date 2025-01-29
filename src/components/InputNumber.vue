108eb3074912774fdcb2e56cbb46977e3c1ee911
katerina <katerina@igloonet.cz>
2025-01-29 11:26:25 +0100

    refactor: new Input components style only

--- src/components/InputNumber.vue
+++ src/components/InputNumber.vue
@@ -2,8 +2,8 @@
 
 <template>
   <div class="md:mt-6 md:mx-6 px-12">
-    <label for="" class="text-start block">Task name</label>
+    <label for="" class="text-start block">Hours to complete</label>
     <!-- <input type="text" id="newTask" v-model="taskInput" /> -->
-    <input type="text" id="" class="border border-white w-full bg-white" />
+    <input type="number" id="" class="border border-white w-full bg-white" />
   </div>
 </template>
