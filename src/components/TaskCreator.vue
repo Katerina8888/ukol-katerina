6406d61b1a8acb8a31d3cd6e30855cd86dbe0da7
katerina <katerina@igloonet.cz>
2025-01-29 11:26:55 +0100

    refactor: new TaskCreator component style only

--- src/components/TaskCreator.vue
+++ src/components/TaskCreator.vue
@@ -1,23 +1,26 @@
 <template>
-  <div>
-    <Input v-model:taskInput="newTask" @addTask="addTask" />
-    <Task :tasks="tasks" @deleteTask="deleteTask" />
+  <div
+    class="flex mx-auto mt-[8rem] justify-center text-center flex-col w-1/4 bg-[#FCC6FF] font-bold py-8 rounded-md"
+  >
+    <!-- <form @submit.prevent="onSubmit"> -->
+    <h1>Task Manager</h1>
+
+    <form class="relative flex flex-col">
+      <Input />
+      <InputNumber />
+      <Button>x</Button>
+    </form>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
 import Input from './Input.vue'
-import Task from './Task.vue'
-
-const tasks = ref<string[]>([])
-const newTask = ref('')
-
-const addTask = (task: string) => {
-  tasks.value.push(task)
-}
-
-const deleteTask = (index: number) => {
-  tasks.value.splice(index, 1)
-}
+import InputNumber from './InputNumber.vue'
+import Button from './Button.vue'
 </script>
+
+<style>
+.sr-only {
+  clip: rect(0, 0, 0, 0);
+}
+</style>
