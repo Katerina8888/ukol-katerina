52b3ad99231876ef1e57603a23fa98acab8d08e7
katerina <katerina@igloonet.cz>
2025-01-30 10:27:09 +0100

    feat: forward emitted events from children in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <div id="app">
-    <TaskCreator />
-    <TasksWrapper />
+    <TaskCreator @on-submit="addTask" />
+    <TasksWrapper :tasks="tasks" @click="removeTask" />
   </div>
 </template>
 
 <script setup lang="ts">
 import TaskCreator from './components/TaskCreator.vue'
 import TasksWrapper from './components/TasksWrapper.vue'
+import { ref } from 'vue'
+import type { Task } from './assets/types'
+
+const tasks = ref([] as Task[]) // Uchovávání seznamu úkolů
+
+const addTask = (task: Task) => {
+  tasks.value.push(task)
+  console.log(tasks.value)
+  // DOTAZ: JAK V KONZOLI ODSTRANIT TY PROXY, ABYCH MĚLA ROVNOU VYPSANÉ POLE?
+}
+
+const removeTask = () => {
+  // tasks.value.pop()
+  console.log(tasks.value)
+}
 </script>
