cd0f28da3274949780f1fc9aae0d5b8a8c75d9fd
katerina <katerina@igloonet.cz>
2025-01-30 10:16:45 +0100

    refactor(button): split into two separate components

---
+++ src/components/ButtonAdd.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="w-16 h-8 mx-auto my-4 rounded-md text-center bg-black text-white hover:bg-white hover:text-black hover:transition-all hover:duration-500 hover:cursor-pointer"
+  >
+    Add
+  </button>
+</template>
---
+++ src/components/ButtonRemove.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="w-4 h-4 text-xs rounded-md text-center bg-black text-white hover:bg-white hover:text-black hover:transition-all hover:duration-500 hover:cursor-pointer"
+  >
+    x
+  </button>
+</template>
