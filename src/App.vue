41567e8d6bcd61a08c8d8a5b5029c47c4188e168
katerina <katerina@igloonet.cz>
2025-01-30 13:44:32 +0100

    feat: add toggle image function

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <div id="app">
-    <TaskCreator @on-submit="addTask" />
+  <div id="app" class="relative">
+    <TaskCreator @on-submit="addTask" @toggle-image="toggleImage" />
     <TasksWrapper :tasks="tasks" @remove-task="removeTask" />
+    <CatImages :showFirstImage="showFirstImage" />
   </div>
 </template>
 
 <script setup lang="ts">
 import TaskCreator from './components/TaskCreator.vue'
 import TasksWrapper from './components/TasksWrapper.vue'
+import CatImages from './components/CatImages.vue'
 import { ref } from 'vue'
 import type { Task } from './assets/types'
 
 const tasks = ref([] as Task[])
+const showFirstImage = ref(true)
 
 const addTask = (task: Task) => {
   tasks.value.push(task)
@@ -21,5 +24,9 @@ const addTask = (task: Task) => {
 const removeTask = (index: number) => {
   tasks.value.splice(index, 1)
   console.log(tasks.value)
+}
+
+const toggleImage = () => {
+  showFirstImage.value = !showFirstImage.value
 }
 </script>
---
+++ src/components/CatImages.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- jsem si vědoma nekvality obrázků, ale nechtěla jsem se víc zdržovat :) -->
+  <div
+    class="hidden md:block md:w-[130px] md:h-[130px] lg:w-[200px] lg:h-[200px] xl:w-[300px] xl:h-[300px] mt-4 absolute right-[3rem] top-[10%]"
+  >
+    <img
+      v-if="showFirstImage"
+      src="../assets/cat3.webp"
+      alt="Cat 1"
+      loading="lazy"
+      width="300"
+      height="300"
+      class="max-w-[100%] h-auto"
+    />
+    <img
+      v-else
+      src="../assets/cat4.gif"
+      alt="Cat 2"
+      loading="lazy"
+      width="300"
+      height="300"
+      class="max-w-[100%] h-auto"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  showFirstImage: boolean
+}>()
+</script>
